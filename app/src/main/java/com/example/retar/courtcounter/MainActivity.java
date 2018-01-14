ec04485ddf5774de9fbf994dc52ad081e4fc3fcf
Charles Rowland <charlesrowland@Charless-MacBook-Pro.local>
2018-01-14 16:48:05 -0700

    goal keeper app for lesson 9

--- app/src/main/java/com/example/retar/courtcounter/MainActivity.java
+++ app/src/main/java/com/example/retar/courtcounter/MainActivity.java
@@ -8,6 +8,8 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
     int scoreTeamA = 0;
     int scoreTeamB = 0;
+    int penaltyTeamA = 0;
+    int penaltyTeamB = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,12 +18,17 @@ public class MainActivity extends AppCompatActivity {
     }
 
     /**
-     * Increase the score for Team A by 3 points.
+     * Reset the game
      */
     public void resetScore(View v) {
         scoreTeamA = 0;
+        scoreTeamB = 0;
+        penaltyTeamA = 0;
+        penaltyTeamB = 0;
         displayForTeamA(scoreTeamA);
         displayForTeamB(scoreTeamA);
+        displayPenaltyTeamA(penaltyTeamA);
+        displayPenaltyTeamB(penaltyTeamB);
     }
 
     /**
@@ -33,19 +40,11 @@ public class MainActivity extends AppCompatActivity {
     }
 
     /**
-     * Increase the score for Team A by 2 points.
+     * Increase the penalties for Team A by 1 point.
      */
-    public void addTwoForTeamA(View v) {
-        scoreTeamA = scoreTeamA + 2;
-        displayForTeamA(scoreTeamA);
-    }
-
-    /**
-     * Increase the score for Team A by 3 points.
-     */
-    public void addThreeForTeamA(View v) {
-        scoreTeamA = scoreTeamA + 3;
-        displayForTeamA(scoreTeamA);
+    public void addPenaltyTeamA(View v) {
+        penaltyTeamA = penaltyTeamA + 1;
+        displayPenaltyTeamA(penaltyTeamA);
     }
 
     /**
@@ -57,6 +56,14 @@ public class MainActivity extends AppCompatActivity {
     }
 
     /**
+     * Displays the given penalties for Team A.
+     */
+    public void displayPenaltyTeamA(int penalty) {
+        TextView penaltyView = (TextView) findViewById(R.id.team_a_penalties);
+        penaltyView.setText(String.valueOf(penalty));
+    }
+
+    /**
      * Increase the score for Team B by 1 point.
      */
     public void addOneForTeamB(View v) {
@@ -65,19 +72,11 @@ public class MainActivity extends AppCompatActivity {
     }
 
     /**
-     * Increase the score for Team B by 2 points.
+     * Increase the penalties for Team B by 1 point.
      */
-    public void addTwoForTeamB(View v) {
-        scoreTeamB = scoreTeamB + 2;
-        displayForTeamB(scoreTeamB);
-    }
-
-    /**
-     * Increase the score for Team B by 3 points.
-     */
-    public void addThreeForTeamB(View v) {
-        scoreTeamB = scoreTeamB + 3;
-        displayForTeamB(scoreTeamB);
+    public void addPenaltyTeamB(View v) {
+        penaltyTeamB = penaltyTeamB + 1;
+        displayPenaltyTeamB(penaltyTeamB);
     }
 
     /**
@@ -86,5 +85,13 @@ public class MainActivity extends AppCompatActivity {
     public void displayForTeamB(int score) {
         TextView scoreView = (TextView) findViewById(R.id.team_b_score);
         scoreView.setText(String.valueOf(score));
+    }
+
+    /**
+     * Displays the given penalties for Team B.
+     */
+    public void displayPenaltyTeamB(int penalty) {
+        TextView penaltyView = (TextView) findViewById(R.id.team_b_penalties);
+        penaltyView.setText(String.valueOf(penalty));
     }
 }
